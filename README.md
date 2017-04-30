d0e1060e93aa936c0bccd7321d15a0819b8a8f09
Lorenzo Lobba <lorenzo@lobba.it>
2017-05-01 00:47:55 +0200

    create url for download file from version and architecture

--- README.md
+++ README.md
@@ -1,21 +1,55 @@
 # Script to create Alpine docker images from scratch
 
-Root file system was taken from https://www.alpinelinux.org/downloads/
+Root file system was taken from https://www.alpinelinux.org/downloads/.
+The Docker image is generated with original and ufficial Alpine Linux release.
+The script creates only dockerfile and docker images.
+It doesn't modify any file of root file system.
 
 The Root file system was signed by "Natanael Copa<ncopa@alpinelinux.org>"
 (His public key and his digest is available on https://www.alpinelinux.org/downloads/)
 
-to create Dockerfile and image
+## Description
+The script makes these steps:
+1. import Natanael Copa public key
+2. take root file system from alpinelinux.org (tar.gz form)
+3. take sha256 digest and check root file system
+4. take file signature  and check root file system
+5. Generate Dockerfile
+6. run Docker build with generated Dockerfile
+7. Delete files taken from alpineLinux. Dockerfile is not deleted.
+
+## Requirements
+1. A standard Linux distribution (Debian, Fedora, ...)
+2. gpg o gpg2
+3. wget
+4. Docker (However the part of the script calling docker command can be skipped.)
+
+## Howto execute script
+To create Dockerfile and image
 
 ```
-./create_alpinebit.sh [-a [x86|x86_64]] | [-f <file name>] | [-d] 
+./create_alpinebit.sh
 ```
-The default values are 
-architecture: x86_64
-file: Dockerfile
 
-Script takes rootfs from repository, creates Dockerfile and creates Docker images.
-With -d option script doesn't create Docker images.
+To show help online
+```
+./create_alpine -h
+create_alpine [options] [arguments]
+        -r <root file system version>   Root File System Version.
+        -a x86|x86_64   choose architecture (default x86_64).
+        -f <file name>  name of generated Dockerfile (default Dockerfile).
+        -d              generate only Dockerfile and don't delete download files.
+        -q              quiet. No message to standard output.
+        -h              show this help.
+```
+## Parameters description
+### Parameters with arguments
+The default values are:
+- -r alpine root file system version: 3.5.2
+- -a architecture: x86_64
+- -f Dockerfile name: Dockerfile
 
-Warning: If you don't use dockerfile default name, take clean manually working directory. 
-
+### Parameters without arguments
+- -d Don't execute steps 6 and 7. Script doesn't remove files and create docker image.
+- -q Execute without any message to standard output (For scripts)
+-h show help
--- create_alpine.sh
+++ create_alpine.sh
@@ -1,121 +1,136 @@
 #!/bin/bash
-#
 
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables:
-dockerfile="Dockerfile"
-repofile="x86_64_alpine.lst"
-stop="NO"
+name="alpine"
+version="3.5.2"
 arch="x86_64"
+dockerfile="Dockerfile"
+stop="NO"
+quiet="NO"
 logfile="log"
 
+keyserver="pgp.mit.edu"
+keyid="0482D84022F52DF1C4E7CD43293ACD0907D9495A"
+
 function show_help {
-	echo -e "create_alpine [options]"
-	echo -e "\t-a x86|x86_64\tchoose architecture (default x86_64)"
-	echo -e "\t-f <file name>\tname of generated Dockerfile (default Dockerfile)"
-	echo -e "\t-d \tgenerate only Dockerfile"
-	echo -e "\t-h show this help"
+	echo -e "create_alpine [options] [arguments]"
+	echo -e "\t-r <root file system version>\tRoot File System Version."
+	echo -e "\t-a x86|x86_64\tchoose architecture (default x86_64)."
+	echo -e "\t-f <file name>\tname of generated Dockerfile (default Dockerfile)."
+	echo -e "\t-d \t\tgenerate only Dockerfile and don't delete download files."
+	echo -e "\t-q \t\tquiet. No message to standard output."
+	echo -e "\t-h \t\tshow this help."
 }
+
+function error {
+	tput setaf 1
+	echo "Opsss... something went wrong! View log file"
+	tput sgr0
+	echo "FAIL! "$(date -Ins)>> $logfile
+	exit 1
+}
+
 function no_docker_image {
 	echo "Warning: Due to -d option, script stops here. None docker image has been created." | tee -a $logfile
-	echo "END "$(date -Ins) | tee -a $logfile
-	exit 0
 }
-while getopts "h:?:a:f:d" opt; do
-    case "$opt" in
-    h|\?)
-        show_help
-        exit 0
-        ;;
-    a)  arch=$OPTARG
-        ;;
-    f)  dockerfile=$OPTARG
-        ;;
-    d)  stop="YES"
-        ;;
-    esac
+function bad_arch {
+	echo -e $1 "is not a valid or supported architecture!" | tee -a $logfile
+	echo "Only x86 and x86_64 are valid values!"| tee -a $logfile
+}
+
+function doIt {
+	echo "import public key from "$keyserver  | tee -a $logfile
+	gpg2 --keyserver  $keyserver --recv $keyid 2>>$logfile
+	[ ! $? -eq 0 ] && error
+
+###############################################################################
+# download roofs
+	echo "get image from alpinelinux.org" | tee -a $logfile
+	emv=$(echo $version | awk -F'.' '{print $1"."$2}')
+	urlrfs="https://nl.alpinelinux.org/alpine/v"$emv"/releases/"$arch"/alpine-minirootfs-"$version"-"$arch
+	for filetype in "tar.gz" "tar.gz.asc" "tar.gz.sha256"; do
+		wget -nc -a $logfile $urlrfs.$filetype
+	done
+
+	echo "verify image with sha256 digest"  | tee -a $logfile
+	sha256sum -c alpine*sha256* 2>>$logfile
+	[ ! $? -eq 0 ] && error
+	echo "verify image with gpg2"  | tee -a $logfile
+	gpg2 --verify alpine*asc 2>>$logfile
+	[ ! $? -eq 0 ] && error
+
+	#mv alpine-minirootfs-*.tar.gz alpine-minirootfs.tar.gz 2>>$logfile
+
+###############################################################################
+# Create Dockerfile
+	[[ -s $dockerfile ]] && rm $dockerfile
+	echo "Create $dockerfile"  | tee -a $logfile
+	echo | tee -a $logfile
+	tput setaf 5
+	tput setab 3
+	echo "#######################################################"     | tee -a $dockerfile $logfile
+	echo "# Alpine Linux "$version" with "$arch "architecture"         | tee -a $dockerfile $logfile
+	echo "#######################################################"     | tee -a $dockerfile $logfile
+	echo "FROM scratch"                                                | tee -a $dockerfile $logfile
+	echo "LABEL maintainer=\"Lorenzo Lobba <lorenzo@lobba.it>\" \\"    | tee -a $dockerfile $logfile
+	echo "source_rootfs=\"https://www.alpinelinux.org/downloads/\" \\" | tee -a $dockerfile $logfile
+	echo "alpine_version=""\""$name"-"$version"-"$arch"\""             | tee -a $dockerfile $logfile
+	echo "ADD alpine-minirootfs-"$version"-"$arch".tar.gz /"           | tee -a $dockerfile $logfile
+	echo "################### END FILE ###########################"    | tee -a $dockerfile $logfile
+	tput sgr0
+	echo $dockerfile" was created!" | tee -a $logfile
+	echo | tee -a $logfile
+	[ $stop == "YES" ] && return 1
+
+	buildName=$name.$version.$arch
+	echo "Create "$buildName" image:"| tee -a $logfile
+	tput setaf 4
+	docker build -t $buildName -f $dockerfile . | tee -a 2>&1 $logfile
+	tput sgr0
+	[ ! $? -eq 0 ] && error
+
+	echo "remove downloads"  | tee -a $logfile
+	rm alpine-minirootfs*
+	tput bel
+	return 0
+}
+
+echo "START "$(date -Ins)>$logfile
+while getopts "h?r:a:f:dq" opt; do
+	case "$opt" in
+		h|\?)
+		show_help
+		exit 0
+		;;
+		a)  arch=$OPTARG
+		;;
+		r) version=$OPTARG
+		;;
+		f)  dockerfile=$OPTARG
+		;;
+		d)  stop="YES"
+		;;
+		q) quiet="YES"
+	esac
 done
 shift $((OPTIND-1))
 
 [ "$1" = "--" ] && shift
 
 case "$arch" in
-	x86)
-	repofile="x86_alpine.lst"
-	;;
-	x86_64)
+	x86|x86_64)
 	;;
 	*)
-	echo "Not valid or supported architecture!"
-	echo "Only x86 and x86_64 are valid values!"
-	show_help
+	bad_arch $arch
+	error
 	;;
 esac
 
-keyserver="pgp.mit.edu"
-keyid="0482D84022F52DF1C4E7CD43293ACD0907D9495A"
+[ $quiet == "YES" ] && doIt >/dev/null || doIt
+[ $? -eq 1 ] && no_docker_image
+echo "END "$(date -Ins)> $logfile
 
-function error {
-	tput setaf 1
-	echo "Opsss... something went wrong! View log file"  | tee -a $logfile
-	exit 1
-	tput sgr0
-}
-echo "START "$(date -Ins)| tee $logfile
-echo "import public key from "$keyserver  | tee -a $logfile
-gpg2 --keyserver  $keyserver --recv $keyid 2>>$logfile
-[ ! $? -eq 0 ] && error
-
-###############################################################################
-# download roofs
-echo "get image from alpinelinux.org" | tee -a $logfile
-wget -nc -a $logfile -i $repofile
-echo "verify image with sha256 digest"  | tee -a $logfile
-sha256sum -c alpine*sha256* 2>>$logfile
-[ ! $? -eq 0 ] && error
-echo "verify image with gpg2"  | tee -a $logfile
-gpg2 --verify alpine*asc 2>>$logfile
-[ ! $? -eq 0 ] && error
-
-
-name=$(ls alpine-minirootfs-*.tar.gz | cut -d - -f 1)
-version=$(ls alpine-minirootfs-*.tar.gz | cut -d - -f 3)
-arch=$(ls alpine-minirootfs-*.tar.gz | cut -d - -f 4 | cut -d . -f 1)
-
-mv alpine-minirootfs-*.tar.gz alpine-minirootfs.tar.gz 2>>$logfile
-
-###############################################################################
-# Create Dockerfile
-[[ -s $dockerfile ]] && rm $dockerfile
-echo "Create $dockerfile"  | tee -a $logfile
-echo | tee -a $logfile
-tput setaf 5
-tput setab 3
-echo "#######################################################"     | tee -a $dockerfile $logfile
-echo "# Alpine Linux "$version" with "$arch "architecture"         | tee -a $dockerfile $logfile
-echo "#######################################################"     | tee -a $dockerfile $logfile
-echo "FROM scratch"                                                | tee -a $dockerfile $logfile
-echo "LABEL maintainer=\"Lorenzo Lobba <lorenzo@lobba.it>\" \\"    | tee -a $dockerfile $logfile
-echo "source_rootfs=\"https://www.alpinelinux.org/downloads/\" \\" | tee -a $dockerfile $logfile
-echo "alpine_version=""\""$name"-"$version"-"$arch"\""             | tee -a $dockerfile $logfile
-echo "ADD alpine-minirootfs.tar.gz /"                              | tee -a $dockerfile $logfile
-echo "################### END FILE ###########################"    | tee -a $dockerfile $logfile
-tput sgr0
-echo $dockerfile" was created!" | tee -a $logfile
-echo | tee -a $logfile
-[ $stop == "YES" ] && no_docker_image
-
-buildName=$name.$version.$arch
-echo "Create "$buildName" image:"| tee -a $logfile
-tput setaf 4
-docker build -t $buildName -f $dockerfile . | tee -a 2>&1 $logfile
-tput sgr0
-[ ! $? -eq 0 ] && error
-
-echo "remove downloads"  | tee -a $logfile
-rm alpine-minirootfs*
-echo "END "$(date -Ins) | tee -a $logfile
-tput bel
 exit 0
